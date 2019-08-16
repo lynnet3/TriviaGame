6400357e9904437939e389aa08a5d9f794841cd6
Emily Tucker <emilytucker4@yahoo.com>
2019-08-15 23:18:33 -0400

    added the instructions and a strart button to start off the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,55 @@
+
+
 var triviaQs =
     [
     {
         question: "What is the worlds largest freshwater lake",
         answer: [ "Lake Victoria","Lake Taganyika", "Lake Superior", "Lake Malawi"],
         right: "Lake Superior",
-    }
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "What is the worlds larget island?",
+        answer:["New Guinea", "Borneo", "Madagascar", "Greenland" ],
+        right:"Greenland",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "Who is the actress that won the most Oscars as of 2019?",
+        answer:["Meryl Streep", "Bette Davis", "Katharine Hepburn","Ingrid Bergman"],
+        right:"Katharine Hepburn",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "What is the color worn by the winners of each stage of the Tour De France?",
+        answer:["White", "Yellow", "Green", "Polka Dot"],
+        right:"Yellow",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "Who was the famous Russian dancer who changed the face of modern ballet?",
+        answer:["Rudolf Nureyev", "Mikhail Baryshnikov", "Galina Ulanova", "Maya Plisetskaya"],
+        right:"Rudolf Nureyev",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "What is the Shakespear play featuring Shylock?",
+        answer:["The Tempest", "The Merchant of Venice", "All's Well That Ends Well", "The Merry Wives of Windsor"],
+        right: "The Merchant of Venice",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "In the show Dad's Army what's Captain Mainwaring's first name?",
+        answer:["Grant", "Gavin", "George", "Gunner"],
+        right:"George",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "Fried tarantulas, eggs boiled just before they're ready to hatch, live octopus and puffin hearts eaten raw and still warm are all traditional foods",
+        answer:["True", "False"],
+        right:"True",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "What is the most common type of star found in our galaxy?",
+        answer:["Brown Dwarf", "White Dwarf", "Red Dwarf", "Black Dwarf"],
+        right:"Red Dwarf",
+    },
     {
-        question:
-        answer:
-        right:
-    }
+        question: "A 'yawl' is a type of what?",
+        answer:["Clothing", "Fish", "Boat", "Building"],
+        right:"Boat",
+    },
     ]
